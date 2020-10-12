f08fb47786b79908048da04f15c9612212f5b7ff
rebequi <rebk1998@icloud.com>
2020-10-11 19:15:21 -0600

    agrego funciones de js de los filtros, mejoro maquetado de la pagina con animaciones

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const closeCartButton = document.getElementById ("close-cart")
 const menuCart = document.getElementById ("menu")
 const overlay = document.getElementById("overlay")
 
+
 // ==========    Final de los selectores ============= 
 
 //  ===============   Empiezan funciones para el carrito ===================
@@ -14,10 +15,84 @@ const overlay = document.getElementById("overlay")
 
 shoppingCartButton.onclick = () => {
     overlay.classList.remove ("hidden")
+    document.body.classList.add ("no-scroll")
+    menuCart.classList.add ("show-menu")
+
+    
 }
 
 // Funcion para cerrar el carrito
 
  closeCartButton.onclick = () => {
      overlay.classList.add("hidden")
+     document.body.classList.remove("no-scroll")
+     menuCart.classList.remove("show-menu")
+    
  }
+
+
+ // ======================== AQUI EMPIEZAN LOS FILTROS ============================
+
+// Selectores para los filtros
+
+const filterByCategory = document.getElementsByClassName ("checkbox-category")
+const filterByReview =document.getElementsByClassName ("checkbox-review")
+const filterBySearch = document.querySelector ("#search-bar")
+const allProductCards = document.querySelectorAll (".product-card")
+
+
+const someCheckboxIsCheckedForCategory = () => {
+    for (let checkboxCategory of filterByCategory) {
+        if (checkboxCategory.checked) {
+            return true
+        }
+    }
+   return false
+}
+
+console.log (someCheckboxIsCheckedForCategory())
+
+const hideCard = (singleProductCard) =>{
+    return singleProductCard.classList.add ("hidden")
+}  
+
+const showCard = (singleProductCard) => {
+    return singleProductCard.classList.remove ("hidden")
+}
+
+const productCardsFilter = ()=> {
+   for (let singleProductCard of allProductCards) {
+       if (passForFilter(singleProductCard)) {
+        showCard (singleProductCard)
+           
+       }
+
+       else {
+        hideCard (singleProductCard)
+           
+       }
+   }
+}
+
+filterBySearch.oninput = () => {
+    productCardsFilter()
+    
+}
+
+
+for (let checkboxCategory of filterByCategory) {
+    checkboxCategory.oninput =()=> {
+        productCardsFilter()
+
+    }
+
+}
+
+for (let checkboxReview of filterByReview) {
+    checkboxReview.oninput =()=> {
+        productCardsFilter()
+
+     }
+
+}
+
