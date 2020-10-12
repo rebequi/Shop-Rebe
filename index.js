14b00c53ca71f1fb8ccdd0225e9611b247b74b2e
rebequi <rebk1998@icloud.com>
2020-10-11 22:28:04 -0600

    agrego funciones de js de los filtros, mejoro maquetado de la pagina con animaciones

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 // ==========    Inicio de los selectores ============= 
 
-const shoppingCartButton = document.getElementById ("view-cart")
-const closeCartButton = document.getElementById ("close-cart")
-const menuCart = document.getElementById ("menu")
+const shoppingCartButton = document.getElementById("view-cart")
+const closeCartButton = document.getElementById("close-cart")
+const menuCart = document.getElementById("menu")
 const overlay = document.getElementById("overlay")
 
 
@@ -14,31 +14,78 @@ const overlay = document.getElementById("overlay")
 // Funcion para abrir el carrito
 
 shoppingCartButton.onclick = () => {
-    overlay.classList.remove ("hidden")
-    document.body.classList.add ("no-scroll")
-    menuCart.classList.add ("show-menu")
+    overlay.classList.remove("hidden")
+    document.body.classList.add("no-scroll")
+    menuCart.classList.add("show-menu")
 
-    
+
 }
 
 // Funcion para cerrar el carrito
 
- closeCartButton.onclick = () => {
-     overlay.classList.add("hidden")
-     document.body.classList.remove("no-scroll")
-     menuCart.classList.remove("show-menu")
-    
- }
+closeCartButton.onclick = () => {
+    overlay.classList.add("hidden")
+    document.body.classList.remove("no-scroll")
+    menuCart.classList.remove("show-menu")
+
+}
 
 
- // ======================== AQUI EMPIEZAN LOS FILTROS ============================
+// ======================== AQUI EMPIEZAN LOS FILTROS ============================
 
 // Selectores para los filtros
 
-const filterByCategory = document.getElementsByClassName ("checkbox-category")
-const filterByReview =document.getElementsByClassName ("checkbox-review")
-const filterBySearch = document.querySelector ("#search-bar")
-const allProductCards = document.querySelectorAll (".product-card")
+const filterByCategory = document.getElementsByClassName("checkbox-category")
+const filterByReview = document.getElementsByClassName("checkbox-review")
+const filterBySearch = document.querySelector("#search-bar")
+const allProductCards = document.querySelectorAll(".product-card")
+
+
+
+const passForFilter =(singleProductCard)=>{
+    if (somethingWrittenOnTheInput()) {
+        if (compareInputWithSingleCard(singleProductCard)) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    else {
+        return true
+    }
+}
+
+
+const hideCard = (singleProductCard) => {
+    return singleProductCard.classList.add("hidden")
+}
+
+const showCard = (singleProductCard) => {
+    return singleProductCard.classList.remove("hidden")
+}
+
+const productCardsFilter = () => {
+    for (let singleProductCard of allProductCards) {
+        if (passForFilter(singleProductCard)) {
+            showCard(singleProductCard)
+        }
+
+        else {
+            hideCard(singleProductCard)
+        }
+    }
+}
+
+const compareInputWithSingleCard = (singleProductCard) => {
+    if (singleProductCard.dataset.nombre.includes(filterBySearch.value.toLowerCase())) {
+        return true
+    }
+    else {
+        return false
+    }
+
+}
 
 
 const someCheckboxIsCheckedForCategory = () => {
@@ -47,52 +94,52 @@ const someCheckboxIsCheckedForCategory = () => {
             return true
         }
     }
-   return false
+    return false
 }
 
-console.log (someCheckboxIsCheckedForCategory())
+const someCheckboxIsCheckedForReview = () => {
+    for (let checkboxReview of filterByReview) {
+        if (checkboxReview.checked) {
+            return true
+        }
 
-const hideCard = (singleProductCard) =>{
-    return singleProductCard.classList.add ("hidden")
-}  
-
-const showCard = (singleProductCard) => {
-    return singleProductCard.classList.remove ("hidden")
+    }
+    return false
 }
 
-const productCardsFilter = ()=> {
-   for (let singleProductCard of allProductCards) {
-       if (passForFilter(singleProductCard)) {
-        showCard (singleProductCard)
-           
-       }
-
-       else {
-        hideCard (singleProductCard)
-           
-       }
-   }
+const somethingWrittenOnTheInput = () => {
+    if (filterBySearch.value){
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 filterBySearch.oninput = () => {
-    productCardsFilter()
-    
+    // productCardsFilter()
+    for (let singleProductCard of allProductCards) {
+        console.log(compareInputWithSingleCard(singleProductCard))
+    }
+
+
 }
 
-
 for (let checkboxCategory of filterByCategory) {
-    checkboxCategory.oninput =()=> {
-        productCardsFilter()
-
+    checkboxCategory.oninput = () => {
+        // productCardsFilter()
+        console.log(someCheckboxIsCheckedForCategory())
     }
 
 }
 
 for (let checkboxReview of filterByReview) {
-    checkboxReview.oninput =()=> {
-        productCardsFilter()
+    checkboxReview.oninput = () => {
+        console.log(someCheckboxIsCheckedForReview())
+        //     productCardsFilter()
 
-     }
+    }
 
 }
+
 
